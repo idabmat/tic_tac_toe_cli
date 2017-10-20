c8dcc602b158f510cf40273d1144f48fe58126b0
Igor de Alcantara Barroso <idabmat@gmail.com>
2017-10-20 15:03:51 +0100

    Add computer cell

--- test/cli/ui_test.exs
+++ test/cli/ui_test.exs
@@ -3,41 +3,67 @@ defmodule Cli.UiTest do
 
   test "displays an empty grid" do
     assert Cli.Ui.display_grid() == """
-            |            |
-            |            |
             |            |            
-            |            |
-            |            |
-            |            |
-            |            |
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
 ____________|____________|____________
-            |            |
-            |            |
-            |            |
-            |            |
-            |            |
-            |            |
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
 ____________|____________|____________
-            |            |
-            |            |
-            |            |
-            |            |
-            |            |
-            |            |
-            |            |
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+            |            |            
+    """
+  end
+
+  test "can display an O in a cell" do
+    assert Cli.Ui.cell(:computer) == """
+            
+    ____    
+  ,' __ `.  
+ / ,'  `. \ 
+ | | () | | 
+ \ `.__,' / 
+  `.____,'  
+            
     """
   end
 
   test "can display an X in a cell" do
-    assert Cli.Ui.cell(:computer) == """
+    assert Cli.Ui.cell(:player1) == """
+            
+   .____,   
+  . \  / ,  
+  |`-  -'|  
+  |,-  -.|  
+  ' /__\ `  
+   '    `   
+            
+    """
+  end
 
-    ____
-  ,' __ `.
- / ,'  `. \
- | | () | |
- \ `.__,' /
-  `.____,'
-
+  test "can display empty cell" do
+    assert Cli.Ui.cell(nil) == """
+            
+            
+            
+            
+            
+            
+            
+            
     """
   end
 
